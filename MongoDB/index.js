1f58f59ed80dfb0aec012ddec634fdfd05b1ceb1
abdulalimrakib <abdulalimrakib04@gmail.com>
2024-01-07 00:21:35 +0600

    create Schema & Model

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -3,12 +3,26 @@ const mongoose = require("mongoose");
 
 const DBcannect = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    await mongoose.connect("mongodb://127.0.0.1:27017/Products");
     console.log("Connected!");
   } catch (error) {
     console.log("Not Connected !!");
   }
 };
+
+//Mongoose Schema
+const Product = new mongoose.Schema({
+  name: String,
+  price: Number,
+  description: String,
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Mongoose Model
+const Products = mongoose.model("Products", Product)
 
 const PORT = 3000;
 
