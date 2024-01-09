04dd52f3ce3c6b2f67ab0701033d5891b8e9bb3d
abdulalimrakib <abdulalimrakib04@gmail.com>
2024-01-09 16:28:10 +0600

    delete from server

--- MongoDB/app.js
+++ MongoDB/app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// mongoose Schema
 const Product = new mongoose.Schema({
   name: String,
   price: Number,
@@ -29,6 +30,7 @@ app.post("/products", async (req, res) => {
     const description = req.body.description;
 
     const newProduct = new Products({
+      // Products = `line 19 const "Products"` =
       name,
       price,
       description,
@@ -57,28 +59,38 @@ app.get("/products", async (req, res) => {
 // $ne not equal
 // $gte grater then equal
 // $lte less then equal
-// $in 
+// $in
 // $nin
 
 // $or
 // $and
 
 // find().countDocuments()                 count the nimber of all documents
-// find().sort({/.../: 1 or -1})           1 for ascending & -1 for descending 
+// find().sort({/.../: 1 or -1})           1 for ascending & -1 for descending
 // find().countDocuments()                 count the nimber of all documents
 
 // selete({/.../})                         which value i want to send {1 for true & 0 for false}
 
-
 app.get("/products/:id", async (req, res) => {
   try {
     const id = req.params.id;
-    const product = await Products.findOne({ _id: id },{name:1, _id:0}); // id base single product
+    const product = await Products.findOne({ _id: id }, { name: 1, _id: 0 }); // id base single product
     if (product) {
       res.status(200).send(product);
     }
   } catch (error) {
     res.status(404).res({ message: error.message });
+  }
+});
+
+//Data Deleting
+app.delete("/products/:id", async(req, res) => {
+  try {
+    const id = req.params.id;
+    const deletedProduct = await Products.deleteOne({ _id: id });
+    res.status(202).send(deletedProduct)
+  } catch (error) {
+    res.status(404).send({ message: error.message });
   }
 });
 
